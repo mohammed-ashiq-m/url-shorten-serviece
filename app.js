4d937b488d51da0a16e6de895008158eb99485db
mohammed-ashiq-m <ashiqmvkpadi@gmail.com>
2019-11-02 13:45:17 +0530

    functions added for shorten the url

--- app.js
+++ app.js
@@ -1,20 +1,99 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var bodyParser = require('body-parser');
+var bases = require('bases');
+var url = require('url');
 var app = express();
+var Db = require('mongodb').Db,
+    Server = require('mongodb').Server;
+app.use(express.static(__dirname + '/public', {
+    maxAge: 20
+}));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var db = new Db('test', new Server('localhost', 27017));
+// Establish connection to db
+db.open(function(err, db) {
+    if(err)console.log(err);
+    db.createCollection('url_db', function(err) {
+        if(err)console.log(err);
+    });
+});
+app.post('/request', function(req, res) {
+    var link = req.body.link;
+    var parseurl = url.parse(link).pathname.slice(1);
+    var hostname = url.parse(link).hostname;
 
-module.exports = app;
+
+    var collection = db.collection('url_db');
+    //collection.remove();
+    if (hostname == "localhost") {
+        collection.find({
+            "s_url": parseurl
+        }).toArray(function(err, item) {
+            if(err)console.log(err);
+            //console.log(item.length);
+            if (item.length == 1) {
+                res.send(item[0].l_url);
+            } else {
+                res.send("URL not found");
+            }
+        });
+    } else {
+        collection.count(function(err, count) {
+            if(err)console.log(err);
+            collection.find({
+                "l_url": link
+            }).toArray(function(err, item) {
+                if(err)console.log(err);
+                if (item.length == 1) {
+                    res.send("Already Created <br/> http://localhost:3000/" + item[0].s_url);
+                } else {
+                    var num = 479890;
+                    var s_url_digits = Number(num) + Number(count);
+                    //console.log(s_url_digits);
+                    var shorturl = bases.toBase36(s_url_digits);
+
+                    var doc1 = {
+                        "s_url": shorturl,
+                        "l_url": link,
+                        "visits": 0
+                    };
+                    collection.insert(doc1);
+                    res.send("http://localhost:3000/" + shorturl);
+                }
+            });
+
+        });
+    }
+});
+app.route("/:url").all(function(req, res) {
+    var req_url = req.params.url;
+    var collection = db.collection('url_db');
+    collection.find({
+        "s_url": req_url
+    }).toArray(function(err, item) {
+        if(err)console.log(err);
+        if (item.length == 1) {
+            var visits=Number(item[0].visits);
+            collection.update({"s_url": req_url,"visits":visits}, {$set:{"s_url": req_url,"visits":visits+1}});
+            res.status(301);
+            res.setHeader("Cache-Control", "no-cache");
+            res.set("Location", item[0].l_url);
+            res.send();
+        } else {
+            res.send("URL not found");
+        }
+    });
+});
+app.get('/result/urls', function(req,res) {
+    console.log("Ajax Request"+req.url);
+    var collection = db.collection('url_db');
+    collection.find({}).sort({"visits":-1}).limit(10).toArray(function(err,data){
+        if(err) console.log(err);
+        res.send(data);
+    });
+});
+app.listen(3000);
---
+++ public/javascripts/client.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $.ajax({
+        url: '/result/urls',
+        type: 'get',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            $("#result").html("<h5>Visited sites :</h5>");
+
+            var arrayLength = data.length;
+            for (var i = 0; i < arrayLength; i++) {
+                $("#result").append("<tr><td><a href='http://localhost:3000/" + data[i].s_url + "'>http://localhost:3000/" + data[i].s_url + "</a></td><td><strong>"+data[i].visits+"</strong></td></tr>");
+                //$("#result").append(");
+            }
+            $("#result").append("</table>")
+        }
+    });
+});
+$("#linkForm").submit(function(event) {
+    event.preventDefault();
+    var link = $("#link").val();
+    if (/^([a-z]([a-z]|\d|\+|-|\.)*):(\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?((\[(|(v[\da-f]{1,}\.(([a-z]|\d|-|\.|_|~)|[!\$&'\(\)\*\+,;=]|:)+))\])|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=])*)(:\d*)?)(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*|(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)){0})(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(link)) {
+    } else {
+        alert("invalid url");
+    }
+    $.ajax({
+        url: '/request',
+        data: {
+            'link': link
+        },
+        type: 'post',
+        success: function(data) {
+            console.log(data);
+            $("#info").html(data);
+        }
+    });
+});
